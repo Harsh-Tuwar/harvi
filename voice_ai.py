3d062ef632ea4dbfb4850fe4121a95dae9c94996
Harsh Tuwar <tuwarharsh08@gmail.com>
2020-09-06 14:22:01 -0300

    more stuff added

--- voice_ai.py
+++ voice_ai.py
@@ -51,8 +51,8 @@ def takeCommand():
 		return statement
 
 print('Initializing Harvi, your AI personal assistant...')
-speak('Initializing Harvi, your AI personal assistant...')
 playsound('./robot_start.mp3')
+speak('Initializing Harvi, your AI personal assistant...')
 wishMe()
 
 if __name__ == '__main__':
@@ -78,8 +78,12 @@ if __name__ == '__main__':
 			speak(results)
 
 		# accessing the web
-		if 'open' in statement:
-			websiteName = (statement.split("open", 1)[1].strip()).replace(" ", "")
+		elif 'open' in statement or "search" in statement:
+			if 'open' in statement:
+				websiteName = (statement.split("open", 1)[1].strip()).replace(" ", "")
+			if 'search' in statement:
+				websiteName = (statement.split("search", 1)[1].strip()).replace(" ", "")
+			print(websiteName)
 			url = f"https://www.{websiteName}.com"
 			response = requests.get(url)
 			if (response.status_code == 200):
@@ -91,25 +95,66 @@ if __name__ == '__main__':
 			time.sleep(3)
 
 		# time prediction
-		if 'time' in statement:
+		elif 'time' in statement:
 			strTime = datetime.datetime.now().strftime("%H:%M:%S")
 			print(f"the time is {strTime}")
 			speak(f"the time is {strTime}")
 
-		if "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
+		elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
 			speak("I was built by Harsh")
 			print("I was built by Harsh")
 
-		if "who is harsh" in statement or "who is he" in statement:
+		elif "who is harsh" in statement or "who is he" in statement:
 			webbrowser.open_new_tab(f"https://whoistuwar.now.sh")
 			speak("Here is Harsh's personal website. You can find more about him over here.")
 			print("Here is Harsh's personal website. You can find more about him over here.")
-			
-		# search stuff
-		elif 'search' in statement:
-			statement = statement.replace("search", "")
-			webbrowser.open_new_tab(statement)
-			time.sleep(2)
+        
+		elif 'camera' in statement or 'capture' in statement or 'click' in statement:
+			ec.capture(0, "robo cam", "img.jpg")
 
-        # elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
-            
+		elif "weather" in statement:
+			api_key="8ef61edcf1c576d65d836254e11ea420"
+			base_url="https://api.openweathermap.org/data/2.5/weather?"
+			speak("whats the city name")
+			city_name=takeCommand()
+			complete_url=base_url+"appid="+api_key+"&q="+city_name
+			response = requests.get(complete_url)
+			x=response.json()
+			
+			if x["cod"]!="404":
+				y=x["main"]
+				current_temperature = y["temp"]
+				current_humidiy = y["humidity"]
+				z = x["weather"]
+				weather_description = z[0]["description"]
+				speak(" Temperature in kelvin unit is " +
+						str(current_temperature) +
+						"\n humidity in percentage is " +
+						str(current_humidiy) +
+						"\n description  " +
+						str(weather_description))
+				print(" Temperature in kelvin unit = " +
+						str(current_temperature) +
+						"\n humidity (in percentage) = " +
+						str(current_humidiy) +
+						"\n description = " +
+						str(weather_description))
+			else:
+				speak(" City Not Found ")
+
+		# elif 'ask' in statement:
+        #     speak('I can answer to computational and geographical questions and what question do you want to ask now')
+        #     question=takeCommand()
+        #     app_id="R2K75H-7ELALHR35X"
+        #     client = wolframalpha.Client('R2K75H-7ELALHR35X')
+        #     res = client.query(question)
+        #     answer = next(res.results).text
+        #     speak(answer)
+            #subprocess.call(["shutdown", "/l"])
+        #     print(answer)
+
+		elif "log off" in statement or "sign out" in statement:
+			speak('Ok , your pc will log off in 10 sec make sure you exit from all applications')
+			subprocess.call(["shutdown", "/l"])
+
+time.sleep(2)
